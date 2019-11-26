e4c9306bf63dadefd41f0053b418bccb836c296a
Rdescamps <rdescamps@unis.fr>
2019-11-26 14:38:55 +0100

    projet

---
+++ ProjetJava/src/main/java/controller/MainController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+@Controller
+public class MainController {
+ 
+	
+    @GetMapping({"/", "/index"})
+    public String index(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+        model.addAttribute("name", name);
+        return "index";
+    }
+ 
+}
