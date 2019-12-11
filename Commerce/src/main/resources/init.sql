f20669aa3c50d0235acbe2369fcbc67654501690
Rdescamps <rdescamps@unis.fr>
2019-12-11 13:43:34 +0100

    initsql

--- Commerce/src/main/resources/init.sql
+++ Commerce/src/main/resources/init.sql
@@ -1,10 +1,30 @@
 CREATE TABLE Client
 (
-    id int(11) NOT NULL AUTO_INCREMENT,
+    id integer NOT NULL,
     nom varchar(100) NOT NULL,
     prenom varchar(100) NOT NULL,
     adresse varchar(100) NOT NULL,
     telephone varchar(100) DEFAULT NULL,
     email varchar(100) NOT NULL,
     PRIMARY KEY (id)
+);
+
+CREATE TABLE Product
+(
+    id integer NOT NULL,
+    name varchar(100) NOT NULL,
+    description varchar(100) NOT NULL,
+    lastUpdate varchar(100) NOT NULL,
+    price varchar(100) DEFAULT NULL,
+    category integer NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (category)
+);
+
+CREATE TABLE Category
+(
+    id integer NOT NULL,
+    name varchar(100) NOT NULL,
+    PRIMARY KEY (id)
+    
 );
