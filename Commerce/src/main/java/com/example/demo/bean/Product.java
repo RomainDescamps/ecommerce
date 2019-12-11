a7d5522ec635155b3e49fe0ff0efd4b48679f7ff
regnard <regnarddavidreign@gmail.com>
2019-12-11 13:25:06 +0100

    ADD properties for product

--- Commerce/src/main/java/com/example/demo/bean/Product.java
+++ Commerce/src/main/java/com/example/demo/bean/Product.java
@@ -1,11 +1,35 @@
 package com.example.demo.bean;
 
+import java.util.Date;
+
 public class Product {
 	
+	private int id;
 	private String name;
 	private String description;
+	private Date lastUpdate;
 	private double price;
+	private Category category;
 	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Date getLastUpdate() {
+		return lastUpdate;
+	}
+	public void setLastUpdate(Date lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+	public Category getCategory() {
+		return category;
+	}
+	public void setCategory(Category category) {
+		this.category = category;
+	}
 	public String getName() {
 		return name;
 	}
