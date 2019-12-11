8cdec8119e89bcfd66e4a16eddd5196e12c954eb
Requ8m <pierredu62610@hotmail.fr>
2019-12-11 13:50:59 +0100

    Ajout classe Category et CategoryMapper

---
+++ Commerce/src/main/java/com/example/demo/bean/Category.java
@@ -0,0 +1,24 @@
+package com.example.demo.bean;
+
+public class Category {
+
+	private int idCategory;
+	private String name;
+
+	public int getIdCategory() {
+		return idCategory;
+	}
+	public void setIdCategory(int idCategory) {
+		this.idCategory = idCategory;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+}
