3e6ab71f6227a3d78916e4998352ee49fff22de2
regnard <regnarddavidreign@gmail.com>
2019-12-11 13:44:11 +0100

    ADD creationProductForm

---
+++ Commerce/src/main/java/com/example/demo/controller/CreerProductController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.demo.bean.Product;
+@Controller
+public class CreerProductController {
+
+}
--- Commerce/src/main/java/com/example/demo/map/ProductMapper.java
+++ Commerce/src/main/java/com/example/demo/map/ProductMapper.java
@@ -1,10 +1,14 @@
 package com.example.demo.map;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.example.demo.bean.Product;
 
 @Mapper
 public interface ProductMapper {
+
+	@Insert("insert into Product(name, description, lastUpdate, price, category) values(#{name},#{description},#{lastUpdate},#{price},#{category})")
+    void insert(Product product);
 
 }
